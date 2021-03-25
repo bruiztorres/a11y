8ac1d7d5efd31e49b82362ccaf150c7396c35c35
Borja Ruiz Torres <bruiz@plainconcepts.com>
2021-03-25 23:40:41 +0100

    Simplify app component structure

--- src/app/core/layout/index.ts
+++ src/app/core/layout/index.ts
@@ -1,4 +1,4 @@
-
+export { MainComponent } from './main/main.component';
 export { FooterComponent } from './footer/footer.component';
 export { HeaderComponent } from './header/header.component';
 export { SidebarComponent } from './sidebar/sidebar.component';
