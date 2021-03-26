229ac9750a6dd9de9e8ba90f876deb6c4198c7fd
Borja Ruiz Torres <bruiz@plainconcepts.com>
2021-03-26 01:14:19 +0100

    Implement spa router solution

--- src/app/tasks/router-task/router-manager/router-manager.component.ts
+++ src/app/tasks/router-task/router-manager/router-manager.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-router-manager',
   template: '<router-outlet></router-outlet>'
 })
-export class RouterManagerComponent {
-  constructor() {
+export class RouterManagerComponent implements OnDestroy {
+  private destroyed$ = new Subject();
 
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      // NOTE: Filter for fixing routing in this
+      // specific task not needed in case you want
+      // to fix the routing globally
+      filter((event: NavigationEnd) => event.url.startsWith('/task/routing')),
+      takeUntil(this.destroyed$)
+    ).subscribe(this.setFocusInFirstHeading);
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
+
+  private setFocusInFirstHeading(): void {
+    const heading = document.querySelector('main h1') as HTMLElement;
+
+    if (!heading) {
+      return;
+    }
+
+    heading.setAttribute('tabindex', '-1');
+    heading.focus();
   }
 }
